5fbf8c9420fd822997c20129cf606c3d9a3cd8ba
Thibaut <khern92@hotmail.fr>
2016-11-03 16:35:12 +0100

    ENH: minor naming fix

---
+++ recherche_balle_tp1/src/TurtleBot.cpp
@@ -0,0 +1,67 @@
+#include "TurtleBot.hpp"
+#include <vector>
+
+TurtleBot::TurtleBot(ros::NodeHandle nod) :
+
+	
+	//Publishers
+	//pub_enable(noeud.advertise<msg>("topic", 1)),
+
+	//Subcribers
+	subscriberCameraRgbImage_raw(nod.subscribe("/camera/rgb/image_raw", 1, &TurtleBot::callbackCameraRgbImage_raw,this)),
+	subscriberCameraRgbImage_color(nod.subscribe("/camera/rgb/image_color", 1, &TurtleBot::callbackCameraRgbImage_color,this)),
+	subscriberCameraRgbImage_rect_color(nod.subscribe("/camera/rgb/image_rect_color", 1, &TurtleBot::callbackCameraRgbImage_rect_color,this))
+{
+}
+
+//Getters
+sensor_msgs::Image TurtleBot::getCameraRgbImage_raw()
+{
+	return cameraRgbImage_raw;
+}
+
+sensor_msgs::Image TurtleBot::getCameraRgbImage_color()
+{
+	return cameraRgbImage_color;
+}
+
+sensor_msgs::Image TurtleBot::getCameraRgbImage_rect_color()
+{
+	return cameraRgbImage_rect_color;
+}
+
+
+void TurtleBot::callbackCameraRgbImage_raw(const sensor_msgs::Image& msg)
+{
+	cameraRgbImage_raw = msg;
+}
+
+void TurtleBot::callbackCameraRgbImage_color(const sensor_msgs::Image& msg)
+{
+	cameraRgbImage_color = msg;
+}
+
+void TurtleBot::callbackCameraRgbImage_rect_color(const sensor_msgs::Image& msg)
+{
+	cameraRgbImage_rect_color = msg;
+}
+
+
+unsigned char* TurtleBot::convertSensor_msgsImageToRaw(sensor_msgs::Image sensor_msgsImage)
+{
+	std::vector<unsigned char> vector_Raw(sensor_msgsImage.height*sensor_msgsImage.width);
+	vector_Raw=sensor_msgsImage.data;
+	unsigned char* raw = &vector_Raw[0];
+	return raw;	
+}
+
+void TurtleBot::displaySensor_msgsImage(std::string type, sensor_msgs::Image sensor_msgsImage)
+{
+	std::cout<<"--------"<<type<<"---------"<<std::endl;
+  	std::cout<<sensor_msgsImage.height<<std::endl;
+	std::cout<<sensor_msgsImage.width<<std::endl;
+	std::cout<<sensor_msgsImage.encoding<<std::endl;
+	std::cout<<sensor_msgsImage.is_bigendian<<std::endl;
+	std::cout<<sensor_msgsImage.step<<std::endl;
+}
+
---
+++ recherche_balle_tp1/src/TurtleBot.hpp
@@ -0,0 +1,47 @@
+#ifndef _TURTLEBOT_
+#define _TURTLEBOT_
+
+#include <ros/ros.h>
+#include <string>
+#include "sensor_msgs/Image.h"
+
+class TurtleBot 
+{
+	private:
+
+	//Subscribers
+	ros::Subscriber subscriberCameraRgbImage_raw ;
+	ros::Subscriber subscriberCameraRgbImage_color ;
+	ros::Subscriber subscriberCameraRgbImage_rect_color ;
+
+	//Publishers
+	//ros::Publisher ... ;
+
+	//
+	sensor_msgs::Image cameraRgbImage_raw;
+	sensor_msgs::Image cameraRgbImage_color;
+	sensor_msgs::Image cameraRgbImage_rect_color;
+
+	public:
+
+	TurtleBot(ros::NodeHandle nod);
+
+	sensor_msgs::Image getCameraRgbImage_raw();
+	sensor_msgs::Image getCameraRgbImage_color();
+	sensor_msgs::Image getCameraRgbImage_rect_color();
+
+
+	//Callbacks
+	void callbackCameraRgbImage_raw(const sensor_msgs::Image& msg);
+	void callbackCameraRgbImage_color(const sensor_msgs::Image& msg);
+	void callbackCameraRgbImage_rect_color(const sensor_msgs::Image& msg);
+
+
+	unsigned char* convertSensor_msgsImageToRaw(sensor_msgs::Image sensor_msgsImage);
+	void displaySensor_msgsImage(std::string type, sensor_msgs::Image sensor_msgsImage);
+
+
+};
+
+
+#endif
