984f6f88a9a8fcd806c13f3a52a474c424fd0bc0
Tristan Klempka <klempka.tristan@gmail.com>
2016-11-07 10:50:41 +0100

    ENH: define => cst int and clean main.cpp code

--- recherche_balle_tp1/src/TurtleBot.hpp
+++ recherche_balle_tp1/src/TurtleBot.hpp
@@ -8,77 +8,85 @@
 #include "geometry_msgs/Twist.h"
 #include "kobuki_msgs/Sound.h"
 
+//Constantes
+const int SOUND_ON = 0;
+const int SOUND_OFF = 1;
+const int SOUND_RECHARGE = 2;
+const int SOUND_BUTTON = 3;
+const int SOUND_ERROR = 4;
+const int SOUND_CLEANINGSTART = 5;
+const int SOUND_CLEANINGEND = 6;
+
 class TurtleBot 
 {
-	private:
-
-	//Subscribers
-	ros::Subscriber subscriberCameraRgbImageRaw;
-	ros::Subscriber subscriberCameraRgbImageColor;
-	ros::Subscriber subscriberCameraRgbImageRectColor;
-	ros::Subscriber subscriberJointStates;
-
-	//Publishers
-	ros::Publisher publisherMobileBaseCommandsVelocity;
-	ros::Publisher publisherMobileBaseCommandsSound;
-
-
-	//Messages
-	sensor_msgs::Image cameraRgbImageRaw;
-	sensor_msgs::Image cameraRgbImageColor;
-	sensor_msgs::Image cameraRgbImageRectColor;
-	sensor_msgs::JointState jointStates;
-
-	kobuki_msgs::Sound mobileBaseCommandsSound;
-	geometry_msgs::Twist mobileBaseCommandsVelocity;
-
-	public:
-
-	TurtleBot(ros::NodeHandle node);
-
-	//Getters
-	sensor_msgs::Image getCameraRgbImageRaw();
-	sensor_msgs::Image getCameraRgbImageColor();
-	sensor_msgs::Image getCameraRgbImageRectColor();
-	geometry_msgs::Twist getMobileBaseCommandsVelocity();
-	sensor_msgs::JointState getJointStates();
-
-	//Setters
-	void setMobileBaseCommandsVelocity(float linearX, float linearY, float linearZ, float angularX, float angularY, float angularZ);
-	void setMobileBaseCommandsSound(int sound);
+    
+private:
+    
+    //Subscrbers
+    ros::Subscriber subscriberCameraRgbImageRaw;
+    ros::Subscriber subscriberCameraRgbImageColor;
+    ros::Subscriber subscriberCameraRgbImageRectColor;
+    ros::Subscriber subscriberJointStates;
+    //Publishers
+    ros::Publisher publisherMobileBaseCommandsVelocity;
+    ros::Publisher publisherMobileBaseCommandsSound;
 
 
-	//Callbacks
-	void callbackCameraRgbImageRaw(const sensor_msgs::Image& msg);
-	void callbackCameraRgbImageColor(const sensor_msgs::Image& msg);
-	void callbackCameraRgbImageRectColor(const sensor_msgs::Image& msg);
-	void callbackJointStates(const sensor_msgs::JointState& msg);
+    //Messages
+    sensor_msgs::Image cameraRgbImageRaw;
+    sensor_msgs::Image cameraRgbImageColor;
+    sensor_msgs::Image cameraRgbImageRectColor;
+    sensor_msgs::JointState jointStates;
 
-	//Publications
-	void sendMobileBaseCommandsVelocity();
-	void sendMobileBaseCommandsSound();
+    kobuki_msgs::Sound mobileBaseCommandsSound;
+    geometry_msgs::Twist mobileBaseCommandsVelocity;
 
-	//Image convertion
-	unsigned char* convertSensor_msgsImageToRaw(sensor_msgs::Image sensorMsgsImage);
+public:
 
-	//Displays
-	void displaySensorMsgsImage(std::string type, sensor_msgs::Image sensorMsgsImage);
-	void displayMobileBaseCommandsVelocity();
-	void displayJointStates();
-	void displayMobileBaseCommandsSound();
+    TurtleBot(ros::NodeHandle node);
 
-	//Motions
-	void stop();
-	void moveForward();
-	void moveBackward();
-	void turnRight();
-	void turnLeft();
-	void moveForwardTurningRight();
-	void moveForwardTurningLeft();
-	void moveBackwardTurningRight();
-	void moveBackwardTurningLeft();
+    //Getters
+    sensor_msgs::Image getCameraRgbImageRaw();
+    sensor_msgs::Image getCameraRgbImageColor();
+    sensor_msgs::Image getCameraRgbImageRectColor();
+    geometry_msgs::Twist getMobileBaseCommandsVelocity();
+    sensor_msgs::JointState getJointStates();
+
+    //Setters
+    void setMobileBaseCommandsVelocity(float linearX, float linearY, float linearZ, float angularX, float angularY, float angularZ);
+    void setMobileBaseCommandsSound(int sound);
+
+
+    //Callbacks
+    void callbackCameraRgbImageRaw(const sensor_msgs::Image& msg);
+    void callbackCameraRgbImageColor(const sensor_msgs::Image& msg);
+    void callbackCameraRgbImageRectColor(const sensor_msgs::Image& msg);
+    void callbackJointStates(const sensor_msgs::JointState& msg);
+
+    //Publications
+    void sendMobileBaseCommandsVelocity();
+    void sendMobileBaseCommandsSound();
+
+    //Image convertion
+    unsigned char* convertSensor_msgsImageToRaw(sensor_msgs::Image sensorMsgsImage);
+
+    //Displays
+    void displaySensorMsgsImage(std::string type, sensor_msgs::Image sensorMsgsImage);
+    void displayMobileBaseCommandsVelocity();
+    void displayJointStates();
+    void displayMobileBaseCommandsSound();
+
+    //Motions
+    void stop();
+    void moveForward();
+    void moveBackward();
+    void turnRight();
+    void turnLeft();
+    void moveForwardTurningRight();
+    void moveForwardTurningLeft();
+    void moveBackwardTurningRight();
+    void moveBackwardTurningLeft();
 
 };
-
 
 #endif
--- recherche_balle_tp1/src/main.cpp
+++ recherche_balle_tp1/src/main.cpp
@@ -1,53 +1,28 @@
 #include "ros/ros.h"
 #include "TurtleBot.hpp"
 
-#define SOUND_ON 0
-#define SOUND_OFF 1
-#define SOUND_RECHARGE 2
-#define SOUND_BUTTON 3
-#define SOUND_ERROR 4
-#define SOUND_CLEANINGSTART 5
-#define SOUND_CLEANINGEND 6
-
 int main(int argc, char **argv)
 {
-  	std::cout<<"Launching recherche_balle_tp1 ..."<<std::endl;
-  	ros::init(argc, argv, "vision");
-  	ros::NodeHandle node;
-	ros::Rate loop_rate(2); // 2Hz 
+    std::cout<<"Launching recherche_balle_tp1 ..."<<std::endl;
+    ros::init(argc, argv, "vision");
+    ros::NodeHandle node;
+    ros::Rate loop_rate(2); // 2Hz 
 
-  	TurtleBot turtleBot(node);
+    TurtleBot turtleBot(node);
 
-	//turtleBot.moveForward();
-	//turtleBot.moveBackward();
-	//turtleBot.turnRight();
-	//turtleBot.turnLeft();
-	//turtleBot.moveForwardTurningRight();
-	//turtleBot.moveForwardTurningLeft();
-	//turtleBot.moveBackwardTurningRight();
-	//turtleBot.moveBackwardTurningLeft();
-
-	//turtleBot.setMobileBaseCommandsSound(SOUND_ON);
-
-  	while(ros::ok())
+    while(ros::ok())
 	{
-		//Displays
-		turtleBot.displaySensorMsgsImage("RAW", turtleBot.getCameraRgbImageRaw());
-		turtleBot.displaySensorMsgsImage("COLOR", turtleBot.getCameraRgbImageColor());
-		turtleBot.displaySensorMsgsImage("COLOR_RECT", turtleBot.getCameraRgbImageRectColor());
-		turtleBot.displayMobileBaseCommandsVelocity();
-		turtleBot.displayJointStates();
-		turtleBot.displayMobileBaseCommandsSound();
+	    //Displays
+	    turtleBot.displaySensorMsgsImage("RAW", turtleBot.getCameraRgbImageRaw());
+	    turtleBot.displaySensorMsgsImage("COLOR", turtleBot.getCameraRgbImageColor());
+	    turtleBot.displaySensorMsgsImage("COLOR_RECT", turtleBot.getCameraRgbImageRectColor());
+	    turtleBot.displayMobileBaseCommandsVelocity();
+	    turtleBot.displayJointStates();
+	    turtleBot.displayMobileBaseCommandsSound();
 
-		//Publications
-		turtleBot.sendMobileBaseCommandsVelocity();
-		//turtleBot.sendMobileBaseCommandsSound();
-
-		//Launching Callbacks and synchronizing with loop_rate
-		ros::spinOnce(); 
-		loop_rate.sleep();
+	    //Launching Callbacks and synchronizing with loop_rate
+	    ros::spinOnce(); 
+	    loop_rate.sleep();
 	}
-  	
-
-  	return 0;
+    return 0;
 }
