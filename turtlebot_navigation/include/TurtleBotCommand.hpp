f59bfab78a8cc5f401c54a39811aed0321cc34fb
Thiabult LAGOUTE <lagoute.31@gmail.com>
2017-01-12 19:10:23 +0100

    ADD: path following command

--- turtlebot_navigation/include/TurtleBotCommand.hpp
+++ turtlebot_navigation/include/TurtleBotCommand.hpp
@@ -4,7 +4,11 @@
 #include <ros/ros.h>
 #include <string>
 #include <vector>
+#include <cmath>
 #include "geometry_msgs/Twist.h"
+#include "nav_msgs/Odometry.h"
+#include "sensor_msgs/Imu.h"
+#include "sensor_msgs/JointState.h"
 
 const float ROBOT_LINEAR_MAX_VELOCITY = 0.25;
 const float ROBOT_ANGULAR_MAX_VELOCITY = 2;
@@ -18,10 +22,16 @@ private:
         
     //Publishers
     ros::Publisher publisherMobileBaseCommandsVelocity;
+	ros::Publisher publisherOdom;
+	ros::Publisher publisherIm;
+	ros::Publisher publisherWheel;
     
     //Messages
     geometry_msgs::Twist mobileBaseCommandsVelocity;
-        
+	nav_msgs::Odometry odom;    
+	sensor_msgs::Imu im;    
+	sensor_msgs::JointState wheel;
+
     ros::NodeHandle& m_node;
 public:
 
@@ -32,14 +42,22 @@ public:
     void stop();
     void move(const float linearVelocity);
     void turn(const float angularVelocity);
+	void TurtleBotCommand::folcom(const float tabp[][], const float linearVelocity, const float angularVelocity);
 
     //Diplays
-    void displayMobileBaseCommandsVelocity();
+    void displayOdom();
     void moveAndTurn(const float linearVelocity, const float angularVelocity);
 
 private:
     geometry_msgs::Twist getMobileBaseCommandsVelocity();
-    void setMobileBaseCommandsVelocity(const float linearX, const float linearY, const float linearZ, const float angularX, const float angularY, const float angularZ);
+	sensor_msgs::JointState TurtleCommand::Wheel();
+	sensor_msgs::Imu TurtleCommand::getOdom();
+	
+	void TurtleBotCommand::setOdom(const float posecov, const float twist);
+	void TurtleBotCommabd::setWheel(const float whp, const float whv, const float whef);
+	void TurtleBotCommand::setOdom(const float linearX, const float linearY, const float linearZ, const float angularX, const float angularY, const float angularZ, const float posX, const float posY, const float posZ, const float angleX, const float angleY, const float angleZ, const float anglew)
+
+
     
 };
 
--- turtlebot_navigation/src/TurtleBotCommand.cpp
+++ turtlebot_navigation/src/TurtleBotCommand.cpp
@@ -10,45 +10,110 @@ TurtleBotCommand::TurtleBotCommand(ros::NodeHandle& node):
 TurtleBotCommand::~TurtleBotCommand()
 {}
 
+
 geometry_msgs::Twist TurtleBotCommand::getMobileBaseCommandsVelocity() 
 {
     return mobileBaseCommandsVelocity;
 }
 
-void TurtleBotCommand::setMobileBaseCommandsVelocity(const float linearX, const float linearY, const float linearZ, const float angularX, const float angularY, const float angularZ)
-{
-    mobileBaseCommandsVelocity.linear.x=linearX;
-    mobileBaseCommandsVelocity.linear.y=linearY;
-    mobileBaseCommandsVelocity.linear.z=linearZ;
-    mobileBaseCommandsVelocity.angular.x=angularX;
-    mobileBaseCommandsVelocity.angular.y=angularY;
-    mobileBaseCommandsVelocity.angular.z=angularZ;
-    publisherMobileBaseCommandsVelocity.publish(mobileBaseCommandsVelocity);
-}
 
-
-void TurtleBotCommand::displayMobileBaseCommandsVelocity()
+void TurtleBotCommand::displayOdom()
 {
-    std::cout<<mobileBaseCommandsVelocity<<std::endl;
+    std::cout<<odom<<std::endl;
 }
 
 //Motions
 void TurtleBotCommand::stop()
 {
-    TurtleBotCommand::setMobileBaseCommandsVelocity(0, 0, 0, 0, 0, 0);
+    TurtleBotCommand::setOdom(0, 0, 0, 0, 0, 0);
 }
 
 void TurtleBotCommand::move(const float linearVelocity)
 {
-    TurtleBotCommand::setMobileBaseCommandsVelocity(linearVelocity, 0, 0, 0, 0, 0);
+    TurtleBotCommand::setodom(linearVelocity, 0, 0, 0, 0, 0);
 }
 
 void TurtleBotCommand::turn(const float angularVelocity)
 {
-    TurtleBotCommand::setMobileBaseCommandsVelocity(0, 0, 0, 0, 0, angularVelocity);
+    TurtleBotCommand::setOdom(0, 0, 0, 0, 0, angularVelocity);
 }
 
 void TurtleBotCommand::moveAndTurn(const float linearVelocity, const float angularVelocity)
 {
-    TurtleBotCommand::setMobileBaseCommandsVelocity(linearVelocity, 0, 0, 0, 0, angularVelocity);
+    TurtleBotCommand::setOdom(linearVelocity, 0, 0, 0, 0, angularVelocity);
 }
+
+/************************Follow Command************************/
+sensor_msgs::Imu TurtleCommand::getOdom()
+{
+	return odom;
+}
+
+sensor_msgs::JointState TurtleCommand::getWheel()
+{
+	return wheel;
+}
+
+void TurtleBotCommand::setOdom(const float linearX, const float linearY, const float linearZ, const float angularX, const float angularY, const float angularZ)
+{
+	odom.twist.linear.x=linearX;	 // speed linear
+	odom.twist.linear.y=linearY;
+	odom.twist.linear.z=linearZ;
+	odom.twist.angular.x=angularX;
+	odom.twist.angular.y=angularY;
+	odom.twist.angular.z=angularZ;	 // speed angular
+	publisherOdom.publish(odom);
+}
+
+void TurtleBotCommabd::setWheel(const float whp, const float whv, const float whef)
+{
+    wheel.position=whp;
+    wheel.velocity=whv;
+    wheel.effort=whef;
+    wheel.publish(wheel);
+}
+
+// For each point, calculate difference between p and p+1
+void TurtleBotCommand::folcom(float tabp[][], const float linearVelocity, const float angularVelocity) 
+{
+	int T=max(size(tabp);
+	float distx=0, disty=0, alpha=0;
+	float px0=0, py0=0, theta0=0, px1=0, py1=0, theta1=0;
+	float pxe=0, pye=0, thetae=0;
+	for(int i=1;i<T-1; i++)
+	{
+		distx=tabp[1][i]-tabp[1][i-1];
+		disty=tabp[2][i]-tabp[2][i-1];
+
+		alpha= acos(distx/sqrt(distx^2+disty^2));
+		
+		if(alpha<0) alpha=-alpha;
+		
+		if(distx>=0 && disty>=0) alpha=alpha-pi/2;
+		else if(distx<=0 && disty>=0) alpha=pi/2-alpha; 
+		else if(distx<=0 && disty<=0) alpha=alpha+pi/2;
+		else if(distx>=0 && disty<=0) alpha=-alpha-pi/2;
+
+		px0=odom.pose.position.x;
+		py0=odom.pose.position.y;
+		theta0=odom.pose.orientation.z;
+
+		TurtleBotCommand::setodom(linearVelocity, 0, 0, 0, 0, angularVelocity);
+
+		px1=odom.pose.position.x-px0;
+		py1=odom.pose.position.y-py0;
+		theta1=odom.pose.orientation.z-alpha0;
+	
+		pxe=px1-distx;
+		pye=py1-disty;
+		thetae=theta1-alpha;
+
+		if(pxe!=0)
+		if(pye!=0)
+		if(thetae!=0)
+	}
+}
+
+
+
+
