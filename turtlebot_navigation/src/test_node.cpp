2601c7d37a9166f55ed937fc7d70cef0891764bd
BrunoDato <bruno.dato.meneses@gmail.com>
2017-01-16 18:21:39 +0100

    ADD: goal publisher for navigation

--- turtlebot_navigation/src/test_node.cpp
+++ turtlebot_navigation/src/test_node.cpp
@@ -18,9 +18,10 @@ int main(int argc, char **argv)
     ros::Publisher pubPath(node.advertise<nav_msgs::Path>("/nav/PathToFollow", 1));
     ros::Publisher pubLocation(node.advertise<nav_msgs::Odometry>("/nav/Location", 1));
     ros::Publisher pubCmdFinished(node.advertise<std_msgs::Bool>("/nav/CommandFinished", 1));
+    ros::Publisher pubGoal(node.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1));
 
     nav_msgs::Path path;
-    geometry_msgs::PoseStamped pose1, pose2, pose3;
+    geometry_msgs::PoseStamped pose1, pose2, pose3, goal;
     nav_msgs::Odometry location;
     std_msgs::Bool commandFinished;
     
@@ -59,6 +60,19 @@ int main(int argc, char **argv)
     location.pose.pose.orientation.y = 2.05;
     location.pose.pose.orientation.z = 2.05;
     location.pose.pose.orientation.w = 2.05;
+    
+    
+    
+    goal.header.seq = 1;
+    goal.header.stamp = ros::Time::now();
+    goal.header.frame_id = "map";
+    goal.pose.position.x = 3;
+    goal.pose.position.y = 2;
+    goal.pose.position.z = 0;
+    goal.pose.orientation.x = 0;
+    goal.pose.orientation.y = 0;
+    goal.pose.orientation.z = 0;
+    goal.pose.orientation.w = 1;
 
     commandFinished.data = true;
     float time = 0;
@@ -66,18 +80,20 @@ int main(int argc, char **argv)
     {
         ros::spinOnce();
         
-        pubLocation.publish(location);
+        //pubLocation.publish(location);
         //std::cout<<location<<std::endl;
         
-        if(time>5)
+        if(time>=5 && time<5.5)
         {
-            pubPath.publish(path);
+            //pubPath.publish(path);
             //std::cout<<path<<std::endl;
+            pubGoal.publish(goal);
+            std::cout<<goal<<std::endl;
         }
         
         if(time>10)
         {
-            pubCmdFinished.publish(commandFinished);
+            //pubCmdFinished.publish(commandFinished);
         }
         
         
