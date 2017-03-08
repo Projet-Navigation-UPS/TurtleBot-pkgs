fcd994dad2ef4e7018ecae06b62fdd9960b930a4
BrunoDato <bruno.dato.meneses@gmail.com>
2017-03-08 19:54:17 +0100

    ENH: graph creation with edges

--- turtlebot_proj_nav/src/directedGraph.cpp
+++ turtlebot_proj_nav/src/directedGraph.cpp
@@ -3,7 +3,8 @@
 #include <iostream>
 #include <ros/ros.h>
 #include <ros/package.h>
-#include <boost/graph/directed_graph.hpp>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
 #include <vector>
 #include <string>
 
@@ -23,8 +24,10 @@ struct LinkProperty
     int cost;
 };
 
-// Define the type of the graph - this specifies a bundled property for vertices
-typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, NodeProperty> Graph;
+// Define the type of the graph - this specifies a bundled property for vertices and edges
+typedef boost::property<boost::edge_weight_t, double> EdgeWeightProperty;
+//typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, boost::no_property, EdgeWeightProperty, NodeProperty> Graph;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, NodeProperty, EdgeWeightProperty> Graph;
 
 int main(int argc, char* argv[])
 {
@@ -50,14 +53,10 @@ int main(int argc, char* argv[])
                 {
                     std::cout << "Node : Id= '" << pNode->Attribute("Id") << "', Label='" << pNode->Attribute("Label") << "'" << std::endl;
                     Graph::vertex_descriptor v = boost::add_vertex(g);
-                    /*g[v].id = pNode->Attribute("Id");
+                    g[v].id = atoi(pNode->Attribute("Id"));
                     g[v].label = pNode->Attribute("Label");
-                    g[v].x = pNode->Attribute("PositionX");
-                    g[v].y = pNode->Attribute("PositionY");*/
-                    g[v].id = 1;
-                    g[v].label = pNode->Attribute("Label");
-                    g[v].x = 0.0;
-                    g[v].y = 0.0;
+                    g[v].x = atof(pNode->Attribute("PositionX"));
+                    g[v].y = atof(pNode->Attribute("PositionY"));
                     
                        
                     pNode = pNode->NextSiblingElement( "Node" );
@@ -71,6 +70,17 @@ int main(int argc, char* argv[])
                 while ( pLink )
                 {
                     std::cout << "Link : Source= '" << pLink->Attribute("Source") << "', Target='" << pLink->Attribute("Target") << "'" << std::endl;
+                    EdgeWeightProperty e = atof(pLink->Attribute("Cost"));
+                    /*typedef boost::graph_traits<Graph>::vertex_iterator vertex_iter_edge, v1, v2;
+                    std::pair<vertex_iter_edge, vertex_iter_edge> vertexPairEdge;
+                    
+                    for (vertexPairEdge = vertices(g); vertexPairEdge.first != vertexPairEdge.second; ++vertexPairEdge.first)
+                    {
+                        if(g[*vertexPairEdge.first].id == atoi(pLink->Attribute("Source"))) *v1 = *vertexPairEdge.first;
+                        if(g[*vertexPairEdge.first].id == atoi(pLink->Attribute("Target"))) *v2 = *vertexPairEdge.first;
+                        
+                    }*/
+                    add_edge(boost::vertex(0, g), boost::vertex(3, g), e, g);
                     pLink = pLink->NextSiblingElement( "Link" );
                 }
             }
@@ -78,6 +88,9 @@ int main(int argc, char* argv[])
     }
     else std::cout << "not found" << std::endl;
     
+    
+    
+    // Display vertices
     typedef boost::graph_traits<Graph>::vertex_iterator vertex_iter;
     std::pair<vertex_iter, vertex_iter> vertexPair;
     for (vertexPair = vertices(g); vertexPair.first != vertexPair.second; ++vertexPair.first)
@@ -86,6 +99,15 @@ int main(int argc, char* argv[])
         std::cout << g[*vertexPair.first].label <<  std::endl;
         std::cout << g[*vertexPair.first].x <<  std::endl;
         std::cout << g[*vertexPair.first].y <<  std::endl <<  std::endl;
+    }
+    
+    // Display edges
+    boost::property_map<Graph, boost::edge_weight_t>::type EdgeWeightMap = get(boost::edge_weight_t(), g);
+    typedef boost::graph_traits<Graph>::edge_iterator edge_iter;
+    std::pair<edge_iter, edge_iter> edgePair;
+    for(edgePair = edges(g); edgePair.first != edgePair.second; ++edgePair.first)
+    {
+        std::cout << EdgeWeightMap[*edgePair.first] << " ";
     }
     
     
