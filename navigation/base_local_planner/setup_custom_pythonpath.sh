80d48a991edc9e6d02029da6706bd77f729080e7
Thibaut <khern92@hotmail.fr>
2017-03-09 15:47:16 +0100

    MOD : Renaming package and adding visibility program

--- navigation/base_local_planner/setup_custom_pythonpath.sh
+++
@@ -1,4 +0,0 @@
-# generated from dynamic_reconfigure/cmake/setup_custom_pythonpath.sh.in
-
-PYTHONPATH=/home/projturtlebot/catkin_ws/src/navigation/base_local_planner/devel/lib/python2.7/dist-packages:$PYTHONPATH
-exec "$@"
