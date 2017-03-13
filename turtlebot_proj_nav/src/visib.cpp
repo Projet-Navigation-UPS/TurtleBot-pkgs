04b90c3ca5ce73fd06aad190dbb917790cec9105
Thibaut <khern92@hotmail.fr>
2017-03-13 18:05:20 +0100

    Loop corrections and 3 amers

--- turtlebot_proj_nav/src/visib.cpp
+++ turtlebot_proj_nav/src/visib.cpp
@@ -113,25 +113,25 @@ int main(int argc, char **argv)
 
 	int n1=720;
 	int n2=700;
-	int m=2;
-	int x[]={100,200};
-    	int y[]={100,200};     
-	float t[]={45,45}; 
+	int m=3;
+	int x[]={100,200,150};//colonne
+    	int y[]={100,600,300};//ligne     
+	float t[]={45,-20,-20}; 
 	
-	int i,j,k,l,o,p=1,q=1,r=0;
+	int i,j,k,l,o,p=0,q=0,r=0,s=0,u=0;
 	float dist=1.0 ;
 	int pix[n1][n2];
 	int v[m][m];
 	float yn[m];
-    	float angle[]={0.0,0.0};
-	float alphamax[]={10,20};
-	int distancemax=40;
+    	float angle[]={0.0,0.0,0.0};
+	float alphamax[]={30,30,30};
+	int distancemax=3000;
 	int distancemin=10;
 
 	float distancecm=130;
 	//1px = 0.494134897 cm
 	// x px = 337cm
-	distancemax=distancecm/0.494134897;
+	//distancemax=distancecm/0.494134897;
 	printf("Dmax = %d\n",distancemax);
 	distancemin=35/0.494134897;	
 	printf("Dmin = %d\n",distancemin);
@@ -159,64 +159,69 @@ int main(int argc, char **argv)
                 		cerr << "Impossible d'ouvrir le fichier !" << endl;
 
 
-		for (i=0;i<=n2;i++) // line
+		for (i=0;i<n2;i++) // line
 		{
-			for (j=0;j<=n1;j++) // colonne
+			for (j=0;j<n1;j++) // colonne
 			{
 				pix[i][j]=15;
 				for(k=0;k<m;k++) // amers
 				{
 					
-					if(t[k]>-90 && t[k]<90)
+					if(t[k]>-90 && t[k]<90) //si theta compris entre -90 degre et 90 degre
 						v[k][1]=x[k];
 					else if (t[k]<-90 && t[k]>90)
 						v[k][1]=-x[k];
 						else v[k][1]=0;
 				
-				yn[k]=sin(t[k]);
-				angle[k]=acos(((i-x[k])+((j-y[k])*yn[k]))/(sqrt(pow((i-x[k]),2)+pow((j-y[k]),2))*(sqrt(1+pow(yn[k],2)))))*180/pi;					
+					yn[k]=sin(t[k]);
+					angle[k]=acos(((i-x[k])+((j-y[k])*yn[k]))/(sqrt(pow((i-x[k]),2)+pow((j-y[k]),2))*(sqrt(1+pow(yn[k],2)))))*180/pi;					
 					//printf("Angle : %f \n", angle[k]);
-				dist = sqrt(pow(i-x[k],2)+pow(j-y[k],2));
+					dist = sqrt(pow(i-x[k],2)+pow(j-y[k],2));
 
 					if((dist<distancemax && dist>distancemin) && (angle[k]<alphamax[k] && angle[k]>-alphamax[k]))
 						pix[i][j]-=3;
 					else 
 						if(dist == 0.0)
-							pix[i][j]=0;
+						{	pix[i][j]=0;
+							printf("Amers :\ni=%d\tj=%d\n",i,j);
+							
+						}
 					
 				}
+
 				//if(i<n&&j<n)
 				//printf("%d ", pix[i][j]);
 				//printf(".");
 				
-				p+=1;
-				if(r<1)	
-					fichier << pix[i][j] << " ";
-				if(p>720 && (r<1))
+				q+=1;
+				if(r<1)	//flag pour savoir si l'ecriture s'est deja faite 1 fois
+				{	fichier << pix[i][j] << " " ;
+					p+=1;
+				}
+				if(p>=70 && (r<1))// condition de retour a la ligne
 				{
 					fichier << endl;
 					p=0;
-					q+=1;
+					
 				}
-				if(q>700 && (r<1))        		        
-        			{	
-					fichier.close();
-					printf("Carte genere correctement, fermeture du fichier, veuillez patienter environ 10 sec... \n");
-					r=1;
-					//return 0;
-				}
+				
 			}
+
+			
 			//printf("\n");
 		}
 
-		//printf("Ligne\n");
+		//printf("i=%d\tj=%d\n",i,j); //Verification iterations ligne et colonne
+		//printf("q=%d\n",q); //Verification nb totale d'iterations
+		if(r<1)      		        
+        	{	
+			fichier.close();
+			printf("Carte genere correctement, fermeture du fichier, veuillez patienter environ 10 sec... \n");
+			r=1;
+		}
+		
 		cout << "Ending visibility " << endl;
 		
-		//fichier.close();
-		//printf("Fermeture du fichier");
-		
-        
-				
 		loop_rate.sleep();
     }
 	return 0;
