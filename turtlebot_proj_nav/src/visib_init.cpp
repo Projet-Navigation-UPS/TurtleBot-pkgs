89a2508ba0f1a072555230d59d0e6e6a19c910ed
Thibaut <khern92@hotmail.fr>
2017-03-16 15:07:47 +0100

    Uncomments and correction indent

--- turtlebot_proj_nav/src/visib_init.cpp
+++ turtlebot_proj_nav/src/visib_init.cpp
@@ -35,26 +35,29 @@ using namespace std;
 
 void Ecriture_carte_visib()
 {
-   	int n1=1000;
+   	int n1=900;
 	int n2=1000;
 	int m=6;
-	int x[]={0,0,0,0,0,0};//colonne
-    	int y[]={0,0,0,0,0,0};//ligne     
-	float t[m-1];//={pi/2,pi/2,0,0,0,0}; 
+	int x[]={0,0,0,0,0,0};//position x de l'amer
+    	int y[]={0,0,0,0,0,0};//position y de l'amer     
+	float t[m-1];
 	
-	int i,j,k,l,o,p=0,q=0,r=0,s=0,u=0,w=0,a;
+	int i,j,k,l,o,p=0,r=0,s=0,u=0,w=0,a;
 	float dist=1.0 ;
-	int pix[n1][n2],pix2[n1][n2];
+	int pix[n1][n2];
 	int v[m][m];
 	float yn[m];
 	float x1[5],y1[5];
-    	float angle[m-1];//={0.0,0.0,0.0,0.0,0.0,0.0};
-	float alphamax[m-1];//={pi/2,pi/2,pi/2,pi/2,pi/2,pi/2};
+    	float angle[m-1];
+	float alphamax[m-1];
 	int distancemax;
 	int distancemin;
 
 	float distancem=2;
 	float xn;
+
+	// Conversion m -> pixel
+
 	//1px = 0.494134897 cm
 	// x px = 337cm
 
@@ -64,13 +67,10 @@ void Ecriture_carte_visib()
 	
 	for(a=0;a<m;a++)
 	{	
-		//printf("*************************** x1 = %lf \t y1 = %lf *********************\n",x1[a],y1[a]);
 		x[a]=x1[a]*100/0.494134897;
 		y[a]=y1[a]*100/0.494134897;
-		alphamax[a]=pi/2;
-		//printf("*************************** x1 = %d \t y1 = %d *********************\n",x[a],y[a]);
+		alphamax[a]=pi/4;
 	}
-
 
 	distancemax=distancem*100/0.494134897;
 	ROS_INFO("Dmax = %d",distancemax);
@@ -80,85 +80,68 @@ void Ecriture_carte_visib()
     	ofstream fichier("src/TurtleBot-pkgs/turtlebot_proj_nav/map/visib.pgm", ios::out | ios::trunc);  
 		// ouverture en écriture avec effacement du fichier ouvert
 
-   		if(fichier && (r<1))
-        		{	
-				ROS_INFO("Creation de la carte");
-				fichier << "P2" << endl;
-				fichier << "#Thibaut" << endl;
-				fichier << "#Carte de visibilite" << endl;
-				fichier << n1 << " " << n2 << endl;
-				fichier << "15" << endl;
-			}
-			//else
-                		//cerr << "Impossible d'ouvrir le fichier !" << endl;
-
+   	if(fichier && (r<1))
+        {	
+		//Entete du pgm
+		ROS_INFO("Creation de la carte");
+		fichier << "P2" << endl;
+		fichier << "#Thibaut" << endl;
+		fichier << "#Carte de visibilite" << endl;
+		fichier << n1 << " " << n2 << endl;
+		fichier << "15" << endl;
+	}
+			
     
-		for (i=0;i<n2;i++) // colonne
+	for (i=0;i<n2;i++) // colonne
+	{
+		for (j=0;j<n1;j++) // ligne
 		{
-			for (j=0;j<n1;j++) // ligne
+			pix[j][i]=15;
+			
+			for(k=0;k<m;k++) // boucle pour les amers
 			{
-				pix[j][i]=15;
-				//pix2[i][j]=1000000;
-				for(k=0;k<m;k++) // amers
-				{
+				dist = sqrt(pow(i-y[k],2)+pow(j-x[k],2)); // calcul de la distance du pixel par rapport aux amers
+
+				// calcul de la normale
+				if(t[k]<3*pi/2 && t[k]>pi/2) 
+					xn = 1;
+				else 	xn = -1;				
 					
-					/*if(t[k]>-90 && t[k]<90) //si theta compris entre -90 degre et 90 degre
-						v[k][1]=-y[k];
-					else if (t[k]<-90 && t[k]>90)
-						v[k][1]=x[k];
-						else v[k][1]=90;*/
+				if(t[k]<pi)
+					yn[k] = -(tan(t[k])*xn);
+				else yn[k] = (tan(t[k])*xn);
+					
+				//calcul de l'angle (produit vectoriel)
+				angle[k]=acos(((j-x[k])*xn+((i-y[k])*yn[k]))/(sqrt(pow((j-x[k]),2)+pow((i-y[k]),2))*(sqrt(pow(xn,2)+pow(yn[k],2)))));					
 				
-					//yn[k]=2;//cos(t[k]);
-					
-					dist = sqrt(pow(i-y[k],2)+pow(j-x[k],2));
-
-					// xn = -1; yn[k] = tan(t[k])*xn;
-					if(t[k]<3*pi/2 && t[k]>pi/2)
-						xn = 1;
-					else 	xn = -1;				
-					
-					if(t[k]<pi)
-						yn[k] = -(tan(t[k])*xn);
-					else yn[k] = (tan(t[k])*xn);
-					
-					
-					angle[k]=acos(((j-x[k])*xn+((i-y[k])*yn[k]))/(sqrt(pow((j-x[k]),2)+pow((i-y[k]),2))*(sqrt(pow(xn,2)+pow(yn[k],2)))));					
-					//printf("Angle : %f \n", angle[k]);
-
-					if((dist<=distancemax && dist>=distancemin) && (angle[k]<=alphamax[k] && angle[k]>=-alphamax[k]) && (pix[j][i]!=0) )
+				// Si la distance est comprise entre la distance min et la distance max et dans l'angle de champ de vision de l'amer, le pixel blanc passe en gris	
+				if((dist<=distancemax && dist>=distancemin) && (angle[k]<=alphamax[k] && angle[k]>=-alphamax[k]) && (pix[j][i]!=0) ) 
+				{	
+					pix[j][i]-=3;
+				}
+				else 
+					if(dist == 0.0) //si la distance est nulle ca veut dire que c'est l'amer (on le met noir)
 					{	
-						pix[j][i]-=3;
+						pix[j][i]=0;
 					}
-					else 
-						if(dist == 0.0)
-						{	pix[j][i]=0;
-							//printf("Amers :\ni=%d\tj=%d\n",i,j);
-							
-						}
-				}
+			}
 
-				//if(i<n&&j<n)
-				//printf("%d ", pix[j][i]);
-				
-				q+=1;
-				if(r<1)	//flag pour savoir si l'ecriture s'est deja faite 1 fois
-				{	fichier << pix[j][i] << " " ;
-					p+=1;
-				}
-				if(p>=70 && (r<1))// condition de retour a la ligne
-				{
-					fichier << endl;
-					p=0;		
-				}
-				else if((p<70 && (r==1)))
-				{
-					ROS_INFO("Integer false, image non generee correctement !");
-				}
+			if(r<1)	//flag pour savoir si l'ecriture s'est deja faite 1 fois
+			{	
+				fichier << pix[j][i] << " " ;
+				p+=1;
+			}
+			if(p>=70 && (r<1))// Retour a la ligne chaque 70 termes pour respecter le format P2
+			{
+				fichier << endl;
+				p=0;		
+			}
+			else if((p<70 && (r==1)))
+			{
+				ROS_INFO("Integer false, image non generee correctement !");
 			}
 		}
-
-		//printf("i=%d\tj=%d\n",i,j); //Verification iterations ligne et colonne
-		//printf("q=%d\n",q); //Verification nb totale d'iterations
+	}
 
 		if(r<1)      		        
         	{	
@@ -166,8 +149,8 @@ void Ecriture_carte_visib()
 			ROS_INFO("Creation de la carte terminee \n________________________________________________________");
 			r=1;
 		}
-		
 }
+
 
 table pgm_imread(char *argv)			//reads pgm image
 	{
