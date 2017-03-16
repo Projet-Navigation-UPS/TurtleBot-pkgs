fa3a2c675bcd22e83889b4deaa7bbf3a82f1db0b
Tristan Klempka <klempka.tristan@gmail.com>
2017-03-16 13:12:04 +0100

    ADD: publish marker id when ask

--- turtlebot_proj_nav/src/localisation_node.cpp
+++ turtlebot_proj_nav/src/localisation_node.cpp
@@ -3,11 +3,18 @@
 #include <tf/transform_listener.h>
 #include <std_msgs/Int8.h>
 #include <std_msgs/Empty.h>
+#include <std_msgs/Int16.h>
 #include <iostream>
 
 static const int DEBUG = 0;
- 
-/* 1238B*/
+static const int TIMEOUT_AR_DETEC = 30;
+
+int GLOBAL_SEARCH;
+
+void askForMarkerCallback(const std_msgs::Empty& empty) {
+    GLOBAL_SEARCH = 1;
+}
+
 int main(int argc, char** argv){
     ros::init(argc, argv, "localisation_node");
     ros::NodeHandle node; 
@@ -45,69 +52,74 @@ int main(int argc, char** argv){
     last_time = ros::Time::now();
     geometry_msgs::Transform odom_msg;
     ros::Publisher odom_pub = node.advertise<geometry_msgs::Transform>("/new_odom", 50);
+    
+    ros::Publisher idMarker_pub = node.advertise<std_msgs::Int16>("/nav/loca/markerSeen", 50); // if error -1 else id marker
+    
+    GLOBAL_SEARCH = 0;
+    std_msgs::Int16 id;
+    ros::Subscriber askForMarker = node.subscribe("/nav/HLC/askForMarker", 10, askForMarkerCallback);
     // FIN MESSAGE
-
-    int find = 0 ;
+    
     while(ros::ok()) {
         try
 	    {
-            std::cout << " find : " << find << std::endl ;
+		if(GLOBAL_SEARCH == 1)
+		    {
+			if(li.waitForTransform("/camera_rgb_optical_frame", "/ar_marker_0", ros::Time(0), ros::Duration(TIMEOUT_AR_DETEC))) // wait for tf during 1 min
+			    {
+				GLOBAL_SEARCH = 0 ; // search once
+				li.lookupTransform("/camera_rgb_optical_frame", "/ar_marker_0", ros::Time(0), transform_imageMarker);
 
-	        if(li.waitForTransform("/camera_rgb_optical_frame", "/ar_marker_0", ros::Time(0), ros::Duration(120)) && find == 0) // wait for tf during 1 min
-            {
-                li.lookupTransform("/camera_rgb_optical_frame", "/ar_marker_0", ros::Time(0), transform_imageMarker);
+				tf::Transform transform_cameraMarker;    
+				transform_cameraMarker = transform_cameraImage ;    
+				transform_cameraMarker *= transform_imageMarker ;   
 
-	            tf::Transform transform_cameraMarker;    
-	            transform_cameraMarker = transform_cameraImage ;    
-	            transform_cameraMarker *= transform_imageMarker ;   
+				tf::Transform transform_robotMarker;    
+				transform_robotMarker = transform_robotCamera;
+				transform_robotMarker *= transform_cameraMarker ;
+				transform_robotMarker.setOrigin(tf::Vector3(transform_robotMarker.getOrigin().getX(), transform_robotMarker.getOrigin().getY(), 0)); 
 
-	            tf::Transform transform_robotMarker;    
-	            transform_robotMarker = transform_robotCamera;
-	            transform_robotMarker *= transform_cameraMarker ;
-	            transform_robotMarker.setOrigin(tf::Vector3(transform_robotMarker.getOrigin().getX(), transform_robotMarker.getOrigin().getY(), 0)); 
+				tf::Transform transform_markerRobot;
+				transform_markerRobot = transform_robotMarker.inverse(); 
 
-	            tf::Transform transform_markerRobot;
-	            transform_markerRobot = transform_robotMarker.inverse(); 
-
-	            tf::Transform transform_mapRobot;
-	            transform_mapRobot = transform_mapMarker;
-	            transform_mapRobot *= transform_markerRobot ;
+				tf::Transform transform_mapRobot;
+				transform_mapRobot = transform_mapMarker;
+				transform_mapRobot *= transform_markerRobot ;
                 
-                // br.sendTransform(tf::StampedTransform(transform_mapRobot, ros::Time::now(), "map", "new_odom"));// send TF  transform_mapRobot  map odom
-
-                std::cout << " echo " <<  std::endl ;
-
-                odom_msg.translation.x = transform_mapRobot.getOrigin().getX();           
-                odom_msg.translation.y = transform_mapRobot.getOrigin().getY();            
-                odom_msg.translation.z = transform_mapRobot.getOrigin().getZ();
-                odom_msg.rotation.x = transform_mapRobot.getRotation().getX();            
-                odom_msg.rotation.y = transform_mapRobot.getRotation().getY();
-                odom_msg.rotation.z = transform_mapRobot.getRotation().getZ();
-                odom_msg.rotation.w = transform_mapRobot.getRotation().getW();
-                odom_pub.publish(odom_msg);
-
-
-                
-                if(DEBUG)
-                {
-                    br.sendTransform(tf::StampedTransform(transform_imageMarker, ros::Time::now(), "/camera_rgb_optical_frame", "/ar_marker_0"));
-                    br.sendTransform(tf::StampedTransform(transform_cameraMarker, ros::Time::now(), "/camera_rgb_frame", "/ar_tr"));
-                    br.sendTransform(tf::StampedTransform(transform_robotMarker, ros::Time::now(), "/base_link", "/ar_tr_tr"));
-                    br.sendTransform(tf::StampedTransform(transform_markerRobot, ros::Time::now(), "/marker_0", "/res"));
-                    br.sendTransform(tf::StampedTransform(transform_mapRobot, ros::Time::now(), "map", "odom")); 
-                }
-                find = 1 ;
-                ros::spinOnce();    
-                return 0;
-            }
-
+				odom_msg.translation.x = transform_mapRobot.getOrigin().getX();           
+				odom_msg.translation.y = transform_mapRobot.getOrigin().getY();            
+				odom_msg.translation.z = transform_mapRobot.getOrigin().getZ();
+				odom_msg.rotation.x = transform_mapRobot.getRotation().getX();            
+				odom_msg.rotation.y = transform_mapRobot.getRotation().getY();
+				odom_msg.rotation.z = transform_mapRobot.getRotation().getZ();
+				odom_msg.rotation.w = transform_mapRobot.getRotation().getW();
+				odom_pub.publish(odom_msg);
+				id.data = 0; // TODO generalisation
+				idMarker_pub.publish(id);
+  
+				if(DEBUG)
+				    {
+					br.sendTransform(tf::StampedTransform(transform_imageMarker, ros::Time::now(), "/camera_rgb_optical_frame", "/ar_marker_0"));
+					br.sendTransform(tf::StampedTransform(transform_cameraMarker, ros::Time::now(), "/camera_rgb_frame", "/ar_tr"));
+					br.sendTransform(tf::StampedTransform(transform_robotMarker, ros::Time::now(), "/base_link", "/ar_tr_tr"));
+					br.sendTransform(tf::StampedTransform(transform_markerRobot, ros::Time::now(), "/marker_0", "/res"));
+					br.sendTransform(tf::StampedTransform(transform_mapRobot, ros::Time::now(), "map", "odom"));
+					std::cout << " find " <<  std::endl ; 
+				    }
+			    }
+			else
+			    {
+				GLOBAL_SEARCH = 0;
+				id.data = -1;
+				idMarker_pub.publish(id); //marker not detected
+			    }
+		    }
 	    }
         catch (tf::TransformException ex)
 	    {
-            std::cout << " TransformException " << std::endl;
+		std::cout << " TransformException " << std::endl;
 	    }
-        
-
+        ros::spinOnce(); 
     }
     return 0;
 };
