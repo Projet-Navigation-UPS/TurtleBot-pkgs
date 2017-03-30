1b4919174c65d58b61978eb11a3da80415ee976a
BrunoDatoMeneses <bruno.dato.meneses@gmail.com>
2017-03-30 10:43:39 +0200

    ENH: documentation and minor fixes

--- turtlebot_proj_nav/include/TurtleBotCamera.hpp
+++ turtlebot_proj_nav/include/TurtleBotCamera.hpp
@@ -8,15 +8,6 @@
 #include "kobuki_msgs/Sound.h"
 
 //Constantes
-const int SOUND_ON = 0;
-const int SOUND_OFF = 1;
-const int SOUND_RECHARGE = 2;
-const int SOUND_BUTTON = 3;
-const int SOUND_ERROR = 4;
-const int SOUND_CLEANINGSTART = 5;
-const int SOUND_CLEANINGEND = 6;
-
-
 const int CAMERA_WIDTH = 640;
 const int CAMERA_HEIGHT = 480;
 const int CAMERA_CHANNELS = 3;
@@ -32,17 +23,14 @@ private:
     //Subscrbers
     ros::Subscriber subscriberCameraRgbImageColor;
 
-    //Publishers
-    ros::Publisher publisherMobileBaseCommandsSound;
-
     //Messages
     sensor_msgs::Image cameraRgbImageColor;
     unsigned char* cameraRgbImageColorRaw;
     std::vector<unsigned char> cameraRgbImageColorVec;
     
-    kobuki_msgs::Sound mobileBaseCommandsSound;
+    // simple callback to receive image from camera 
+    void callbackCameraRgbImageColor(const sensor_msgs::Image& msg);
     
-    ros::NodeHandle& m_node;
 public:
 
     TurtleBotCamera(ros::NodeHandle& node);
@@ -52,22 +40,12 @@ public:
     sensor_msgs::Image getCameraRgbImageColor();
     unsigned char* getCameraRgbImageColorRaw();
  
-    //Setters    
-    void setMobileBaseCommandsSound(const int sound);
 
     //Image conversion
-    unsigned char* convertSensor_msgsImageToRaw(const sensor_msgs::Image& sensorMsgsImage);
     sensor_msgs::Image convertRawToSensorMsgsImage(const unsigned char* raw, const int height, const int width, const std::string&encoding, const char is_bigendian, const int step);
 
-    //Displays
+    //Display
     void displaySensorMsgsImage(const std::string& type, const sensor_msgs::Image& sensorMsgsImage);
-    void displayMobileBaseCommandsSound();
-
-
-private:
-
-    // simple callback to receive image from camera 
-    void callbackCameraRgbImageColor(const sensor_msgs::Image& msg);
     
 };
 
